d9752bda452c3e2bc2b778847f507bbd1980c42f
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-05-18 23:45:20 -0300

    feat: upload csv files

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,28 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
-
     try {
-      // await api.post('/transactions/import', data);
+      const promises = uploadedFiles.map(file => {
+        const data = new FormData();
+        data.append('file', file.file);
+        return api.post('/transactions/import', data);
+      });
+
+      await Promise.all(promises);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    setUploadedFiles(
+      files.map(file => ({
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      })),
+    );
   }
 
   return (
